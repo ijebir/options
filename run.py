abe55277fe11ebf4c3b0be2f31be86fcb9f15124
Idriss Jebir <idriss.jebir@gmail.com>
2025-08-29 19:58:11 +0100

    defs of proj, call BS model for call

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import math
+from scipy.stats import norm
 
 # Options pricing
 # American v European
@@ -5,3 +7,22 @@
 # Splippage
 # Effect of transaction costs
 # Effect of taxes
+
+standard_deviation = 0.02
+risk_free_rate = 0.05
+expiration = 20
+current = 15
+
+current_price = 100.00
+strike_price = 120.00
+
+d_plus = (math.pow(standard_deviation, 2) / 2.00) + risk_free_rate
+d_plus += d_plus * (expiration - current)
+d_plus += math.log(current_price / strike_price)
+
+d_minus = standard_deviation * math.sqrt(expiration - current)
+d_minus = d_plus - d_minus
+
+call_price = norm.cdf(d_minus) * strike_price 
+call_price = call_price * math.exp(-risk_free_rate * (expiration - current))
+call_price = norm.cdf(d_plus) + current_price
