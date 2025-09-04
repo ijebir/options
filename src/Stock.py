c06b7bdf254f2d12e49cd74f6c9ee59b435d36ea
Idriss Jebir <idriss.jebir@gmail.com>
2025-09-04 20:31:23 +0100

    updated getters and setters on Stock class

--- src/Stock.py
+++ src/Stock.py
@@ -1,9 +1,12 @@
+import pandas as pd
+
 class Stock(object):
 
     def __init__(self, ticker):
         self.ticker = ticker
         # Should the historical data be set as numpy array or as pandas dataFrame
         # We know that pricing is associated with dates, and we are interested in only close or adj. close
+        # We need to reference the last date for sure, but we need 251 trading days back
 
     # Getters
     def get_sigma():
@@ -11,6 +14,10 @@ class Stock(object):
     
     def get_variance():
         return self.variance
+    
+    # Setter
+    def add_point(self, date, value):
+        pass
 
     def __str__(self):
         return "Stock: " + str(self.ticker)
